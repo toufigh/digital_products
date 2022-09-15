8a1bb76586d3d47e8eb4a71a110df20c33d5a48c
toufigh <bohamdant@gmail.com>
2022-09-15 05:58:43 +0430

    Products models added.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class Category(models.Model):
+    parent = models.ForeignKey('self', verbose_name=_('parent'), on_delete=models.CASCADE, blank=True, null=True)
+    title = models.CharField(verbose_name=_('title'), max_length=50)
+    description = models.TextField(verbose_name=_('description'), blank=True)
+    avatar = models.ImageField(verbose_name=_('avatar'), blank=True, upload_to='categories/')
+    is_enable = models.BooleanField(verbose_name=_('is enable'), default=True)
+    created_time = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(verbose_name=_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+
+class Product(models.Model):
+    title = models.CharField(verbose_name=_('title'), max_length=50)
+    description = models.TextField(verbose_name=_('description'), blank=True)
+    avatar = models.ImageField(verbose_name=_('avatar'), blank=True, upload_to='products/')
+    is_enable = models.BooleanField(verbose_name=_('is enable'), default=True)
+    categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
+    created_time = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(verbose_name=_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
+
+class File(models.Model):
+    product = models.ForeignKey('Product', verbose_name=_('product'), on_delete=models.CASCADE)
+    title = models.CharField(verbose_name=_('title'), max_length=50)
+    file = models.FileField(verbose_name=_('file'), upload_to='files/%Y/%m/%d')
+    is_enable = models.BooleanField(verbose_name=_('is enable'), default=True)
+    created_time = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(verbose_name=_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'files'
+        verbose_name = _('file')
+        verbose_name_plural = _('files')
